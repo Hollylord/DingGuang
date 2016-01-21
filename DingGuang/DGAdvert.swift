4ab40c6c94df14d1ca9d5d756f9fae5401697517
Hollylord <283228138@qq.com>
2016-01-21 17:53:03 +0800

    桥接OC和Swift文件

---
+++ DingGuang/DGAdvert.swift
@@ -0,0 +1,15 @@
+//
+//  DGAdvert.swift
+//  DingGuang
+//
+//  Created by apple on 16/1/21.
+//  Copyright © 2016年 dingguangiPhone. All rights reserved.
+//
+
+import Foundation
+
+
+class test: NSObject {
+    let mgr: Int = 5
+    
+}
