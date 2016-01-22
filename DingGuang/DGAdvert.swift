e860a8d561818ec447e8c2e1ef103b0b283161bb
Hollylord <283228138@qq.com>
2016-01-22 15:50:27 +0800

    旋转加缩放动画

--- DingGuang/DGAdvert.swift
+++ DingGuang/DGAdvert.swift
@@ -9,7 +9,30 @@
 import Foundation
 
 
-class test: NSObject {
-    let mgr: Int = 5
+class DGAdvertView: UIImageView {
+    //加required是因为这是指定初始化器,表示子类要继承这个方法
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+//        self.image = UIImage(named: "advert1")
+        
+    }
+    
+}
+
+class DGCartoonView: UIImageView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.image = UIImage(named: "design")
+        let scaleTransform = CGAffineTransformMakeScale(3, 3)
+        let rotationTransform = CGAffineTransformMakeRotation( CGFloat(M_PI) )
+        
+        self.transform = CGAffineTransformConcat(scaleTransform ,rotationTransform)
+        
+        UIView.animateWithDuration(0.8, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.transform = CGAffineTransformIdentity
+            }, completion: nil)
+    }
+    
     
 }
